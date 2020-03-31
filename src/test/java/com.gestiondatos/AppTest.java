1840ec3b5326e72f09b346b6ff576c75eca6263d
Lucas Gianotti <lucas.gianotti@mercadolibre.com>
2020-03-31 18:53:25 -0300

    primer commit

---
+++ src/test/java/com.gestiondatos/AppTest.java
@@ -0,0 +1,13 @@
+package com.gestiondatos;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class AppTest {
+
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
